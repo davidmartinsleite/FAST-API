f04d4e895eba3eadf22fd14b322d5aa97a7b46b0
David Martins Leite <davidmartinsleite@hotmail.com>
2022-11-28 20:02:15 -0300

    aula 03 Headers,Cookies

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-from typing import Union  # isso serve para dizer que algo não é obrigatorio, mesma coisa que "optional"
+from typing import Optional, Union  # isso serve para dizer que algo não é obrigatorio, mesma coisa que "optional"
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Response, Cookie
 
 from pydantic import BaseModel  # essa lib serve para pegar dados contidos no corpo de uma requisição
+
 
 class Item(BaseModel):  # essa classe erda de "BaseModel"
     id: int
     descricao: str
     valor: float
 
+
 app = FastAPI()  # nome da aplicação você pode mudar para qualquer nome, lembre de mudar na execução da aplicação: uvicorn main:aplicativo --reload
 
 
 @app.get("/")  # get é um metodo http, você pode mudar para 'post' ou 'delete'
-def read_root():
-    return {"Hello": "World 2"}
+def read_root(usuario: Union[str, None] = Header(None)):  # ele vai criar titulo
+    return {"user-agente": usuario}
+# o "Header" pode retornar por padrão um "none" ou vc pode mudar esse retorno para uma msg
+# caso vc use "user_agent" como saida ele vai retornar os dados do seu navegador !!!
+# def read_root(user_agent: Union[str, None] = Header(None)):
+
+
+@app.get("/cookie")  # quando queremos quardar alguma preferencia do usuario
+def cookie(response: Response):  # vamos usar o "Response" para
+    response.set_cookie(key="meucookie", value="123456")  # ele retorna uma chave e valor, esse valor pode ser a chave da sessão
+    return {'cookie adicionado': True}
+
+
+@app.get('/pegar-Cookie')
+def get_cookie(meucookie: Optional[str] = Cookie(None)):
+    return {'Cookie coletado': meucookie}
+# ele vai pegar o Cookie que esteja salvo no navegador
 
 
 @app.get("/items/{item_id}")
