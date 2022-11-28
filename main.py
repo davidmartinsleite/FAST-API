e5804459f1b041b55319e37616eef3ed26da9ea0
David Martins Leite <davidmartinsleite@hotmail.com>
2022-11-28 19:15:23 -0300

    aula 02 Request body

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from typing import Union  # isso serve para dizer que algo não é obrigatorio, 
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel  # essa lib serve para pegar dados contidos no corpo de uma requisição
+
+class Item(BaseModel):  # essa classe erda de "BaseModel"
+    id: int
+    descricao: str
+    valor: float
+
 app = FastAPI()  # nome da aplicação você pode mudar para qualquer nome, lembre de mudar na execução da aplicação: uvicorn main:aplicativo --reload
 
 
@@ -13,6 +20,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/item")  # esse é o caminho para o endpoint
+def adicionar_item(novo_item: Item, outro_item: Item):  # aqui vamos colocar como parametro a classe "Item"
+    # tambem podemos adicionar mais de um conjunto como o "outro_item" que tambem recebe o "Item"
+    return [novo_item, outro_item]  # retornamos o resultado de "Item", ou os resultados
+
 
 # para iniciar o programa: uvicorn main:app --reload
 # o "--reload" serve para dar auto reload serve para qualquer auteração do codigo ele recarrega automaticamente
